348120058c9d354fd38cc09e8f2c46eb2e4d4636
Rama Fajar Fadhilllah <78191852+ramarff@users.noreply.github.com>
2022-07-09 15:21:35 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/wisatadepok/';
+$config['base_url'] = 'https://kreasi.nurulfikri.ac.id/rama21010ti/wisatadepok/';
 
 /*
 |--------------------------------------------------------------------------
